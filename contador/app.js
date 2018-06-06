fc942a1bf605c609560ac9560490484317d0bba6
Edu Alcover <alcover.edu@gmail.com>
2018-06-06 17:52:58 +0200

    contador

---
+++ contador/app.js
@@ -0,0 +1,8 @@
+window.onload = (function miAlert() {
+    var x = 10;
+    while (x > 0) {
+        alert('Contador: ' + x);
+        x--;
+    }
+    alert('BooooooM');
+});
---
+++ contador/app.ts
@@ -0,0 +1,10 @@
+
+window.onload = (function miAlert():void{
+    let x:number=10;
+    while(x>0){
+         alert('Contador: ' +x)
+         x--;
+    }
+
+    alert('BooooooM');
+})
